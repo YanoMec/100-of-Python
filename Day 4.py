bf47a8b81250a140b5f1a9c804f2be6a54cc8457
YanoMec <cemonay@live.co.uk>
2023-11-23 17:03:29 +0000

    Day 4

---
+++ Day 4.py
@@ -0,0 +1,9 @@
+# Python docs https://www.askpython.com/
+# import random 
+# x = random.randint(range1, range2)
+# e.g random_integer.rantint(1,10) --random number between 1 to 10
+
+# random_float = random.random() -- random float between 0.00 to 1.00
+# random_float * 5  -- expand range to 0.00 to 5.0000  
+
+# you can create list using "[]" e.g x = [item1,item2,item3,item4]
