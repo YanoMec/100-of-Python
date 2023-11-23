34a52b01cf7dd9a79409887164373357a2cac3e7
YanoMec <cemonay@live.co.uk>
2023-11-23 10:17:35 +0000

    Day 3 Challange

--- Day 3.py
+++ Day 3.py
@@ -28,18 +28,43 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************      
      ''' )
 print("Welcome to Treasure Island!\n Your mission, if you choose to accept, is to find the treasure of Black Beard!")
+win = False
+while win==False:
+    cross_road =input('Your at a cross road do you go left or right?\nEnter left or right  ')
+    road = cross_road.lower()
 
-cross_road =input('Your at a cross road do you go left or right?\nEnter left or right ')
-road = cross_road.lower()
 
-if "right" == road:
-    print("You fell into a hole. !!GAME OVER!!")
-  
-else:
-    print("You've arrived at the Islands port, You have two options.\nSwim or wait for a boat.")
-    port = input("Are you going to swim or wait for the boat?\nEnter Swim or Boat")
-    option= port.lower()
-    if "swim" == option:
-        print("You where eatting by a shark !!GAME OVER!!")
-    else:    
-        print("You have arrived at a light house with 2 doors")
+    if road == "right":
+        print("You fell into a hole. !!GAME OVER!!")
+    
+    elif road == "left":
+        print("You've arrived at the Islands port, You have two options.\nSwim or wait for a boat.")
+       
+        port = input("Are you going to swim or wait for the boat?\nEnter Swim or wait  ")
+        option= port.lower()
+       
+        if option == "swim":
+            print("You where eatting by a shark !!GAME OVER!!")
+        if option =="wait":    
+            print("You have arrived at a light house with 3 doors.\nRed, Yellow and Blue")
+
+            door_selection = input("what door do you choose?\nEnter Red, Yellow or Blue  ")
+            door=door_selection.lower()
+       
+            if door == "red":
+                print("you were burned by fire. !!GAME OVER!!")
+
+            if door =="blue":
+                print("You were eaten by beasts. !!GAME OVER!!")
+            if door == "yellow":
+                print("You found the treasure!")
+                win = True
+    else:
+        print("Please Enter left or right ")
+
+     
+   
+   
+
+
+    
