b9515abf70fe0c3c5bd012f2c8d10e702c7f5ddd
YanoMec <cemonay@live.co.uk>
2023-12-06 17:16:55 +0000

    Day 7

---
+++ Day 7.py
@@ -0,0 +1,24 @@
+#Day 7 - Creating Hang man Game. 
+
+#Flow chart Programing. (use drawio)
+
+word_list = ["aardvark", "baboon", "camel"]
+
+
+import random
+chosen_word = random.choice(word_list)
+
+print(f'Pssst, the solution is {chosen_word}.')
+display= ["_" for letter in chosen_word]
+
+     
+print(display)
+user_guess = input("Guess a letter?\n").lower()
+
+for letter in chosen_word:
+    if user_guess in chosen_word:
+        print("Right")
+    else:
+        print("Wrong")
+
+
