a729af0a8ef168186b1fb0c74e8d1b194e37b39b
Cem Onay <Cem.Onay@fdmgroup.com>
2024-01-03 17:27:33 +0000

    Day 7

--- Day 7.py
+++ Day 7.py
@@ -79,7 +79,7 @@ word_length = len(chosen_word)
 lives = 6
 
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+#print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -103,7 +103,8 @@ while not end_of_game:
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lose.")
+            print(f'You lose.\nYour Word was {chosen_word}')
+
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
